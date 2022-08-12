bba8a0f6554bc5945870fcde755689a1cfb90850
Samuel Domin <samuel.domin@dominanz.sk>
2022-08-12 16:07:40 +0200

    pridane pripojenie na databazu v BE

---
+++ BE/db_conn.py
@@ -0,0 +1,14 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+mydb = mysql.connector.connect(
+        host="db.dw082.nameserver.sk",
+        database="filip_soc",
+        user="filip_majchrak",
+        password="X5Me1BZj"   
+    )
+
+if mydb.is_connected():
+        db_Info = mydb.get_server_info()
+        print ("Connected to MySQL Server version", db_Info)
--- BE/temp.py
+++ BE/temp.py
@@ -1,1 +1,3 @@
 print("test");
+
+#tento program sluzi na zapisovanie teplot do db
