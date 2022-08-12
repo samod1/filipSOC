0ce364b40cf2fec106153196e296e342836d5336
Samuel Domin <samuel.domin@dominanz.sk>
2022-08-12 20:13:06 +0200

    pripojenie na DB zo strany FE

--- UI/confs/DB.php
+++ UI/confs/DB.php
@@ -1,3 +1,30 @@
 <?php
-    echo "Alfa Romeo";
+    $server = "db.dw082.nameserver.sk";
+    $user = "filip_majchrak";
+    $pass = "X5Me1BZj";
+    $db = "filip_soc";
+
+    $conn= mysqli_connect($server,$user,$pass,$db);
+
+    if(!$conn)
+    {
+        echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+        Pripojenie na databazu <b>". $server . "</b> zlyhalo
+        <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+          <span aria-hidden='true'>&times;</span>
+        </button>
+      </div>";
+    }
+
+    else
+    {
+        echo "<div class='alert alert-success alert-dismissible fade show' role='alert'>
+        Pripojenie na databazu <b>". $server . "</b> prebehlo uspesne
+        <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+          <span aria-hidden='true'>&times;</span>
+        </button>
+      </div>";
+    }
+
+    //TODO grafana integration
 ?>
