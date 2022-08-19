8705f8d1e6a7af8813a0c7fb3fe4d83a4ca77e0a
Samuel <samko.domin@gmail.com>
2022-08-19 18:40:55 +0200

    Revert "pull request"

--- UI/confs/navbar.php
+++ UI/confs/navbar.php
@@ -13,10 +13,10 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-              <a class="nav-link" href="index.php">Domov<span class="sr-only">(current)</span></a>
+              <a class="nav-link" href="#">Domov<span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="udaje.php">Grafy</a>
+              <a class="nav-link" href="#">Grafy</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Udaje</a>
--- UI/index.php
+++ UI/index.php
@@ -10,26 +10,20 @@
  *  Na uvod suboru zavolat cez prikaz include z adresaru confs head.php a na zaver footer.php 
  */
 
+ /**Namerané hodnoty */
+ $teplota = 30;
+ $tlak = 1014.2;
+ $vlhkost = 20;
+
 
 $nazovTabu ="Filip Stredoskolska praca";
 include "confs/head.php";
 ?>
 
-<?php
-/**Skušobné údaje */
-$teplota = 30;
-$tlak = 1014.2;
-$vlhkost = 20;
-
-?>
-
 <div  class="container-fluid">
     <div class="row">
         <div class="col">
-        <div  class="container-fluid">
-    <div class="row">
-        <div class="col">
-          <h1 class="text-center">Dashboard</h1>
+          <h1 class="text-center">Posledné namerané údaje</h1>
           <!-- Tabulka s nameranými hodnotami-->
             <div id="pricing" class="container-fluid">
               <table class="table table-bordered table-striped text-center">
@@ -83,4 +77,3 @@ function checkTime(i)
   return i;
 }
 </script>
-
