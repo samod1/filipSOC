2a02885042128abfaf0e69c26bd1b812a9d272ca
Samuel Domin <samuel.domin@dominanz.sk>
2022-10-06 20:09:39 +0200

    pridanie generovacieho skriptu

--- BE/db_conn.py
+++ BE/db_conn.py
@@ -1,6 +1,12 @@
 import mysql.connector
 from mysql.connector import Error
+import mysql.connector
+from datetime import datetime
+import random
 
+def randomNumberGenerator():
+    nahodnecislo = random.randint(0,40);
+    return nahodnecislo
 
 mydb = mysql.connector.connect(
         host="db.dw082.nameserver.sk",
@@ -13,4 +19,16 @@ if mydb.is_connected():
         db_Info = mydb.get_server_info()
         print ("Connected to MySQL Server version", db_Info)
 
-#FIXME dokoncit overovanie pre uspesne resp. neuspesne spojenie s databazov
+        cursor=mydb.cursor()
+
+for x in range(400):
+    sql= "INSERT INTO filip_soc.tbl_teplota (value, `timestamp`, miesto_merania, jednotka) VALUES(%s, %s, 500011, 2);"
+    now = datetime.now()
+    timeNow = dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
+    hodnota = randomNumberGenerator()
+    print(timeNow , hodnota)
+    val = (hodnota, timeNow)
+    cursor.execute(sql,val)
+    mydb.commit()
+
+
