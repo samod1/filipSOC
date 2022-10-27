e2d60126d8d2af11b2b514f8723392e1d88c978b
Samuel Domin <samuel.domin@dominanz.sk>
2022-10-27 10:47:33 +0200

    vytvorenie okna #75

---
+++ UI/settings.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * VNUTORNA UNIFORMNA STRUKTURA PROGRAMU
+ * pouzivat bootstrapove divy 
+ *      <div class="col">
+ *          <div class = "row">
+ *              <p>...</p>
+ *          </div>
+ *      <div>
+ *  Na uvod suboru zavolat cez prikaz include z adresaru confs head.php a na zaver footer.php 
+ */
+
+
+$conn = "";
+$nazovTabu ="Filip Stredoskolska praca";
+$stranka = "nastavenia";
+include "confs/head.php";
+
+$prsi = true;
+
+?>
+
+
+<div class="container-fluid">
+    <h1 class="display-4">Nastavenia</h1>
+</div>
+
+<div class="row">
