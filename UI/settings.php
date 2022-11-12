d8d93432135e45f927095fc523c9e94f65b2a149
Filipenzo <76654153+Filipenzo@users.noreply.github.com>
2022-11-12 20:53:43 +0100

    #97 pridana druha cast so senzormi

--- UI/settings.php
+++ UI/settings.php
@@ -11,79 +11,150 @@
 
 
 <div class="container-fluid">
-    <h1 class="display-4">Nastavenia</h1>
-    <br>
-
-    <!--Jednotky-->
-    <div class="row ml-1">
-        <h4 class="display-5 mb-0">Jednotky: &nbsp;</h3>
-        <select class="form-select" aria-label="Jednotky">
-            <option selected value="1">Metricke</option>
-            <option value="2">Imperiálne</option>
-        </select>
-    </div>
-
-    <!--Formát času-->
-    <div class="row ml-1 mt-4">
-        <h4 class="display-5">Časový formát: &nbsp;</h3>
-        <div class="form-check mt-1">
-            <input class="form-check-input" type="radio" name="hod_format" id="24" checked>
-            <label class="form-check-label" for="24">
-               24H &nbsp;
-            </label>
-        </div>
-        <div class="form-check mt-1">
-            <input class="form-check-input" type="radio" name="hod_format" id="12">
-            <label class="form-check-label" for="12">
-                12H &nbsp;
-            </label>
-        </div>
-    </div>
-
-    <!--Miesto merania-->
-    <div class="row ml-1 mt-3">
-        <h4 class="display-5 mb-0">Miesto merania: &nbsp;</h3>
-        <select class="form-select" aria-label="Jednotky">
-            <?php
-            $query_obce = "SELECT kód,názov FROM filip_soc.enum_okres";
-            $result_obce = mysqli_query($conn,$query_obce);
-            $pocetriadkov_obce = mysqli_num_rows($result_obce);
-            if(!$result_obce)
-            {
-                echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
-                <strong>ERR:</strong> Prikaz SQL sa neda vykonat ".$query_obce."
-                <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
-                <span aria-hidden='true'>&times;</span>
-                </button>
-            </div>";
-            }
-            else
-            {
-                if ($pocetriadkov_obce == 0)
-                {
-                echo "<div class='alert alert-warning alert-dismissible fade show' role='alert'>
-                <strong>Holy guacamole!</strong> You should check in on some of those fields below.
-                <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
-                    <span aria-hidden='true'>&times;</span>
-                </button>
-                </div>";
-                }
-            }
+    <div class="row"> 
+        <div class="col-sm">  
+            <h1 class="display-4">Nastavenia</h1>
+            <br>
             
-            while ($row_obce = mysqli_fetch_assoc($result_obce))
-            { 
-            ?>
-                <!--vytvaranie moznosti-->
-                <option value="<?php echo $row_obce["kód"];?>"> <?php echo $row_obce["názov"];?> </option>
-            <?php } ?>
-        </select>
-    </div>
+            <div class="input-group mb-3">
+                <!--Jednotky-->
+                <h4 class="display-5 mb-0">Jednotky: &nbsp;</h3>
+                <select class="form-select" aria-label="Jednotky">
+                    <option selected value="1">Metricke</option>
+                    <option value="2">Imperiálne</option>
+                </select>
+            </div>
 
-    <!--Save-->
-    <div class="row ml-1 mt-5">
-        <button type="button" class="btn btn-primary p-3">Save</button>
-    </div>
+            <div class="input-group mb-3">
+                <!--Formát času-->
+                <h4 class="display-5">Časový formát: &nbsp;</h3>
+                <div class="form-check mt-1">
+                    <input class="form-check-input" type="radio" name="hod_format" id="24" checked>
+                    <label class="form-check-label" for="24">
+                    24H &nbsp;
+                    </label>
+                </div>
+                <div class="form-check mt-1">
+                    <input class="form-check-input" type="radio" name="hod_format" id="12">
+                    <label class="form-check-label" for="12">
+                        12H &nbsp;
+                    </label>
+                </div>
+                <br>
+            </div>    
 
+            <div class="input-group mb-3">
+                <!--Miesto merania-->
+                <h4 class="display-5 mb-0">Miesto merania: &nbsp;</h3>
+                <select class="form-select" aria-label="Jednotky">
+                    <?php
+                    $query_obce = "SELECT kód,názov FROM filip_soc.enum_okres";
+                    $result_obce = mysqli_query($conn,$query_obce);
+                    $pocetriadkov_obce = mysqli_num_rows($result_obce);
+                    if(!$result_obce)
+                    {
+                        echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+                        <strong>ERR:</strong> Prikaz SQL sa neda vykonat ".$query_obce."
+                        <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                        <span aria-hidden='true'>&times;</span>
+                        </button>
+                    </div>";
+                    }
+                    else
+                    {
+                        if ($pocetriadkov_obce == 0)
+                        {
+                        echo "<div class='alert alert-warning alert-dismissible fade show' role='alert'>
+                        <strong>Holy guacamole!</strong> You should check in on some of those fields below.
+                        <button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+                            <span aria-hidden='true'>&times;</span>
+                        </button>
+                        </div>";
+                        }
+                    }
+                    
+                    while ($row_obce = mysqli_fetch_assoc($result_obce))
+                    { 
+                    ?>
+                        <!--vytvaranie moznosti-->
+                        <option value="<?php echo $row_obce["kód"];?>"> <?php echo $row_obce["názov"];?> </option>
+                    <?php } ?>
+                </select>
+            </div> 
+                <!--Save-->
+                <button type="button" class="btn btn-primary p-3">Save</button>
+          
+        </div>
+
+        <!--Senzory-->
+        <div class="col-sm">
+            <h1 class="display-4">Senzory</h1>
+            <!--ADD Senzor-->
+            <button type="button" class="btn btn-primary p-3 pull-right mb-3" data-toggle="modal" data-target="#ADD_sensor">Pridat</button>
+            
+            <!--modalne okno add-->
+            <div class="modal fade" id="ADD_sensor" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLongTitle">Pridanie senzoru</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <!--ID_senzoru-->
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="basic-addon1">Názov senzoru</span>
+                            </div>
+                            <input type="text" name="id_sensor" class="form-control" aria-describedby="id_sensor">
+                        </div>
+
+                        <!--ID_senzoru-->
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="basic-addon1">Vyuzitie</span>
+                            </div>
+                            <input type="text" name="id_sensor" class="form-control" aria-describedby="use_sensor">
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary" data-dismiss="modal">Pridať</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+            <!--Tabulka senzorov-->
+            <table class="table table-hover table-bordered text-center mt-3">
+                <thead>
+                    <tr>
+                        <th scope="col">Senzor_id</th>
+                        <th scope="col-6">Vyuzitie</th>
+                    </tr>
+                </thead>
+                <tbody class="table-active">
+                    <?php
+                    $query_sensor = "SELECT id,sensor_id,pouzitie_senzoru FROM filip_soc.tbl_sensors ts;";
+                    $result_sensor = mysqli_query($conn,$query_sensor);
+                    $pocetriadkov_sensor = mysqli_num_rows($result_sensor);
+
+                    while ($row_sensor = mysqli_fetch_assoc($result_sensor))
+                    {
+                    ?>
+                        <tr>
+                            <td><?php echo $row_sensor["sensor_id"];?></td>
+                            <td><?php echo $row_sensor["pouzitie_senzoru"];?></td>
+                        </tr>
+                    <?php } ?>
+
+                </tbody>
+            </table>
+        </div>
+
+    </div>
 </div>
 
 <?php
