ee510ef97eba7ac1535165d2627e837ad6b698e6
Filipenzo <76654153+Filipenzo@users.noreply.github.com>
2022-11-22 19:39:02 +0100

    hdt11 tutorial

--- BE/temp.py
+++ BE/temp.py
@@ -2,3 +2,5 @@ print("test");
 
 #tento program sluzi na zapisovanie teplot do db
 #TODO napisat program pre meranie teploty
+
+#https://www.freva.com/dht11-temperature-and-humidity-sensor-on-raspberry-pi/
