1bf0f2a3a6756e5954ec3ac16d4b1ffac12c135c
Filipenzo <76654153+Filipenzo@users.noreply.github.com>
2022-12-03 18:01:10 +0100

    #95 pokus o prepojenie s DB

--- UI/settings.php
+++ UI/settings.php
@@ -13,62 +13,98 @@
             <h1 class="display-4">Nastavenia</h1>
             <br>
             
-            <div class="input-group mb-3">
-                <!--Jednotky-->
-                <h4 class="display-5 mb-0">Jednotky: &nbsp;</h3>
-                <select class="form-select" aria-label="Jednotky">
-                    <option selected value="1">Metricke</option>
-                    <option value="2">Imperiálne</option>
-                </select>
-            </div>
-
-            <div class="input-group mb-3">
-                <!--Formát času-->
-                <h4 class="display-5">Časový formát: &nbsp;</h3>
-                <div class="form-check mt-1">
-                    <input class="form-check-input" type="radio" name="hod_format" id="24" checked>
-                    <label class="form-check-label" for="24">
-                    24H &nbsp;
-                    </label>
+            <form action="" method="post">
+                <div class="input-group mb-3">
+                    <!--Jednotky-->
+                    <h4 class="display-5 mb-0">Jednotky: &nbsp;</h3>
+                    <select class="form-select" name="jednotky" aria-label="Jednotky">
+                        <option selected value="1">Metricke</option>
+                        <option value="2">Imperiálne</option>
+                    </select>
                 </div>
-                <div class="form-check mt-1">
-                    <input class="form-check-input" type="radio" name="hod_format" id="12">
-                    <label class="form-check-label" for="12">
-                        12H &nbsp;
-                    </label>
-                </div>
-                <br>
-            </div>    
 
-            <!--Miesto merania-->
-            <h4 class="display-5 mb-3">Miesto merania:<br></h3>
-            <div class="input-group mb-3">
-                <h5 class="display-5 mb-0 mt-1">Okres: &nbsp;</h5>
-                <select class="form-select" name="okres" id="okres" aria-label="Okres" onchange="okres_id()" required>
-                <?php
-                    $query_okres = "SELECT * FROM filip_soc.enum_okres eo WHERE eo.`kód krajiny` = '703';";
-                    $result_okres = mysqli_query($conn,$query_okres);
-                    $pocetriadkov_okres = mysqli_num_rows($result_okres);
+                <div class="input-group mb-3">
+                    <!--Formát času-->
+                    <h4 class="display-5">Časový formát: &nbsp;</h3>
+                    <div class="form-check mt-1">
+                        <input class="form-check-input" type="radio" name="hodformat" value="24" id="hodformat" checked>
+                        <label class="form-check-label">
+                        24H &nbsp;
+                        </label>
+                    </div>
+                    <div class="form-check mt-1">
+                        <input class="form-check-input" type="radio" name="hodformat" value="12" id="hodformat">
+                        <label class="form-check-label">
+                            12H &nbsp;
+                        </label>
+                    </div>
+                    <br>
+                </div>    
+
+                <!--Miesto merania-->
+                <h4 class="display-5 mb-3">Miesto merania:<br></h3>
+                <div class="input-group mb-3">
+                    <h5 class="display-5 mb-0 mt-1">Okres: &nbsp;</h5>
+                    <select class="form-select" name="okres" id="okres" aria-label="Okres" onchange="okres_id()" required>
+                    <?php
+                        $query_okres = "SELECT * FROM filip_soc.enum_okres eo WHERE eo.`kód krajiny` = '703';";
+                        $result_okres = mysqli_query($conn,$query_okres);
+                        $pocetriadkov_okres = mysqli_num_rows($result_okres);
+                        
+                        while ($row_okres = mysqli_fetch_assoc($result_okres))
+                        { 
+                        ?>
+                            <!--vytvaranie moznosti-->
+                            <option value="<?php echo $row_okres["kód"];?>"> <?php echo $row_okres["názov"];?> </option>
+
+                        <?php } ?>
+
+                    </select>
+                </div> 
+
+                <div class="input-group mb-3">
+                    <h5 class="display-5">Obec: &nbsp;</h5>
+                    <select class="form-select" name="obec" id="obec" aria-label="Obec" required></select>
+                    <?php
+                        /*$okr = $_POST["okres"];
+
+                        $query_obce = "SELECT * FROM filip_soc.enum_obce eo2 WHERE eo2.`kód okresu` = 101 ;";
+                        $result_obce = mysqli_query($conn,$query_obce);
+                        $pocetriadkov_obce = mysqli_num_rows($result_obce);
+
+                        while ($row_okres = mysqli_fetch_assoc($result_obce))
+                        { 
+                        ?>
+                            <!--vytvaranie moznosti-->
+                            <option value="<?php echo $row_obce["kód"];?>"> <?php echo $row_obce["názov"];?> </option>
+
+                        <?php } ?>*/
+
+                        
                     
-                    while ($row_okres = mysqli_fetch_assoc($result_okres))
-                    { 
                     ?>
-                        <!--vytvaranie moznosti-->
-                        <option value="<?php echo $row_okres["kód"];?>"> <?php echo $row_okres["názov"];?> </option>
+                </div> 
 
-                    <?php } ?>
+                <!--Save-->    
+                <button type="button" name="save" id="save" class="btn btn-primary p-3">Save</button>
 
-                </select>
-            </div> 
+                <!--Posielanie udajov do tbl_settings-->
+                <!--tento shit nefunguje-->
+                <?php 
+                    if(isset($_POST['save']))
+                    {
 
-            <div class="input-group mb-3">
-                <h5 class="display-5">Obec: &nbsp;</h5>
-                <select class="form-select" name="obec" id="obec" aria-label="Obec" required></select>
-                    <option ></option>
-            </div> 
+                        $id = 0;
+                        $query_save_setings = "INSERT INTO filip_soc.tbl_settings(hod_format,jednotky,miesto_merania) VALUES (?,?,?,?);";
+                        $stmt = mysqli_stmt_init($conn);
+                        mysqli_stmt_prepare($stmt,$query_save_setings);
+                        mysqli_stmt_bind_param($stmt,'iisi', $id, $_POST["hod_format"], $_POST["jednotky"], "703");
+                        mysqli_stmt_execute($stmt);
+                        mysqli_stmt_close($stmt);
 
-            <!--Save-->    
-            <button type="button" class="btn btn-primary p-3">Save</button>
+                    }  
+                ?>
+            </form>
           
         </div>
 
@@ -90,7 +126,7 @@
                             </button>
                         </div>
                         <!--ID_senzoru-->
-                        <form method="post">
+                        <form action="" method="post">
                             <div class="input-group mb-3">
                                 <div class="input-group-prepend">
                                     <span class="input-group-text" id="basic-addon1">Názov senzoru</span>
@@ -103,10 +139,8 @@
                                 <div class="input-group-prepend">
                                     <span class="input-group-text" id="basic-addon1">Vyuzitie</span>
                                 </div>
-                                <input type="text" name="function" id="function" class="form-control" aria-describedby="function_sensor">
-                                <br>
-                                <input type="submit" name="poslat" id="poslat" class="btn btn-primary" value="Odoslať">
-
+                                    <input type="text" name="function" id="function" class="form-control" aria-describedby="function_sensor">
+                                    <input type="submit" name="poslat" id="poslat" class="btn btn-primary" value="Odoslať">
                             </div>
                             <?php
                             
@@ -119,6 +153,7 @@
                                     mysqli_stmt_bind_param($stmt,'iss', $id, $_POST["id_sensor"], $_POST["function"]);
                                     mysqli_stmt_execute($stmt);
                                     mysqli_stmt_close($stmt);
+
                                 }  
                             ?>
 
@@ -157,7 +192,6 @@
         </div>
     </div>
 </div>
-
 
 <?php                       
     include "confs/footer.php";
