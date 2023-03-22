747cb40b815a0b07e21c2d5d4544b99e856da2f6
Samuel Domin <samuel.domin@dominanz.sk>
2023-03-22 19:13:34 +0100

    pridanie noveho programu pre spracovanie udajov z DHT22

---
+++ BE/DHT22_new.py
@@ -0,0 +1,22 @@
+import Adafruit_DHT
+import math
+import time
+import DB_func
+from DB_func import InsertHumidity
+
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, 4)
+    humidity = round(humidity, 2)
+    temperature = round(temperature, 2)
+   
+    if humidity is not None and temperature is not None:
+
+      #print 'Temperatuur: {0:0.1f}*C'.format(temperature)
+      humidityFormated =  '{0:0.1f}'.format(humidity)
+      print(humidityFormated)
+      DB_func.InsertHumidity(humidityFormated)
+     
+
+    else:
+      print ('Geen data')
+    time.sleep(10)
